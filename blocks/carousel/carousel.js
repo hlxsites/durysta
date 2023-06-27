cd48765262ffb47146a4c841e99e309f6ee089fa
Kiran Murugulla <murugull@adobe.com>
2023-06-27 14:39:04 -0400

    adjusting styles for carousel with green box

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -358,9 +358,9 @@ class Carousel {
 
     const itemChildren = [...item.children];
     itemChildren.forEach((itemChild, idx) => {
-      if(itemChild.querySelectorAll('p').length > 1){
-        itemChild.querySelector('p:first-of-type > picture').classList.add('mobile');
-        itemChild.querySelector('p:nth-of-type(2) > picture').classList.add('desktop');
+      if (itemChild.querySelectorAll('p').length > 1) {
+        if (itemChild.querySelector('p:first-of-type > picture')) { itemChild.querySelector('p:first-of-type > picture').classList.add('mobile'); }
+        if (itemChild.querySelector('p:nth-of-type(2) > picture')) { itemChild.querySelector('p:nth-of-type(2) > picture').classList.add('desktop'); }
       }
       if (itemChild.querySelector('img')) {
         itemChild.classList.add('carousel-item-image');
