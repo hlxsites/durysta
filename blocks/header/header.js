b7684e2f9d21ede9de5a20f4c2099ae4241da20d
Peter A <52969899+rootpea@users.noreply.github.com>
2023-07-06 00:48:25 +0200

    45 - header - in page section highlighter

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -231,7 +231,7 @@ export default async function decorate(block) {
       { threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] },
     );
 
-    // Find sections containing each h1 header, store the headier id, and add it to nav detection
+    // Find sections containing each h1 header, store the header id, and add it to nav detection
     document.querySelectorAll('h1').forEach((header) => {
       let highestParent = header;
       let levels = 20;
