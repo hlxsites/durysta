471e2ab081aa3d54e1f0863e350a14af02ee0c35
Peter A <52969899+rootpea@users.noreply.github.com>
2023-07-10 16:49:46 +0200

    set carousel and columns as lcp

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,7 +12,7 @@ import {
   loadCSS,
 } from './lib-franklin.js';
 
-const LCP_BLOCKS = ['carousel', 'columns']; // add your LCP blocks to the list
+const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
  * Builds a section divider
